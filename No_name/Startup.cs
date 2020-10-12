e684435a749c573ceff980929156a6a057c8c1b0
Yura-Potter <55709495+Yura-Potter@users.noreply.github.com>
2020-10-12 13:12:57 +0300

    Innitial commit

---
+++ No_name/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(No_name.Startup))]
+namespace No_name
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
